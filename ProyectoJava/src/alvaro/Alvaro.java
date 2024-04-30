493b588822d701e2ea4a6701482d1e4055add267
Alvaro <alvarovr712@gmail.com>
2024-04-30 18:12:28 +0200

    Alvaro version4

--- ProyectoJava/src/alvaro/Alvaro.java
+++ ProyectoJava/src/alvaro/Alvaro.java
@@ -2,4 +2,15 @@ package alvaro;
 
 public class Alvaro {
 
+	public static void main(String[] args) {
+		
+		Alvaro alvaro = new Alvaro();
+		alvaro.miNombre();
+
+	}
+	
+	public static void miNombre() {
+		System.out.println("Hola soy Alvaro");
+	}
+
 }
