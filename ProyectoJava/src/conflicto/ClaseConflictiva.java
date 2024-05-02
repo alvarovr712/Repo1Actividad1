4a09eea4cdb5429ff75a980445542271739045df
javier <fjzamora93@gmail.com>
2024-05-02 11:59:22 -0400

    clase conflictiva

---
+++ ProyectoJava/src/conflicto/ClaseConflictiva.java
@@ -0,0 +1,10 @@
+package conflicto;
+
+public class ClaseConflictiva {
+
+	public static void main(String[] args) {
+
+		System.out.println("Mi comida favorita es: comida1, comida2, comida3");
+
+	}
+}
