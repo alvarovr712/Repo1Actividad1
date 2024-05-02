eec43d97a31f9fec42b7cf20dfd6f1a41d34ba83
Alvaro <alvarovr712@gmail.com>
2024-05-02 18:06:45 +0200

    Alvaro version7

--- ProyectoJava/src/conflicto/ClaseConflictiva.java
+++ ProyectoJava/src/conflicto/ClaseConflictiva.java
@@ -4,7 +4,7 @@ public class ClaseConflictiva {
 
 	public static void main(String[] args) {
 
-		System.out.println("Mi comida favorita es: comida1, comida2, comida3");
+		System.out.println("Mi comida favorita es: macarrones, paella, chuletón");
 
 	}
 }
